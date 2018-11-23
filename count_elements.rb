504e9c73c0a6942ae5a67f8abc93cd8b006cc99c
Robin Kim <rkim1214@gmail.com>
2018-11-23 20:36:43 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |string|
+    if new_hash[string]
+      new_hash[string] += 1
+    else 
+      new_hash[string] = 1
+    end 
+  end 
+  new_hash
 end
  
